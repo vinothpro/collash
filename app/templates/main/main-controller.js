fa699132ef8258b0cb894da05bbf1f83264d657f
vinothpro <vinothr@thoughtworks.com>
2015-10-24 23:07:50 +0530

    Implemented reset pass

--- app/templates/main/main-controller.js
+++ app/templates/main/main-controller.js
@@ -2,11 +2,16 @@
 
 module.controller("MainController", ["$scope", function($scope) {
     $scope.page = "main";
-    $scope.userMenu = "search"
-    $scope.tab = "account"
+    $scope.userMenu = "search";
+    $scope.changePass = false;
+    $scope.tab = "account";
 
     $scope.closeToggler = function() {
         $scope.footerMenu = "";
+    }
+
+    $scope.resetPassword = function() {
+        $scope.changePass = false;
     }
 
     $scope.closeToggler();
