934430ade0a0c1c43704d58e6f0471811f915352
vinothpro <vinothr@thoughtworks.com>
2015-10-25 18:55:45 +0530

    updated main page

--- app/templates/main/main-controller.js
+++ app/templates/main/main-controller.js
@@ -8,6 +8,11 @@ module.controller("MainController", ["$scope", function($scope) {
     $scope.tab = "account";
     $scope.searchText = "";
     $scope.cardView = false;
+    $scope.mouse = false;
+
+    $scope.navigateMenu = function(menu) {
+        $scope.menu = menu;
+    }
 
     $scope.closeToggler = function() {
         $scope.menu = "search";
@@ -17,5 +22,13 @@ module.controller("MainController", ["$scope", function($scope) {
         $scope.changePass = false;
     }
 
+    $scope.showMenu = function() {
+        $scope.mouse = !$scope.mouse;
+    }
+
     $scope.closeToggler();
+
+    $scope.$watch('menu', function() {
+        console.log($scope.menu)
+    });
 }]);
