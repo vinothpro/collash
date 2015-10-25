45a5707c02436d5addd489995cec7882f6bf8ce1
vinothpro <vinothr@thoughtworks.com>
2015-10-25 16:35:25 +0530

    UI issues in home page

--- app/templates/home/home-controller.js
+++ app/templates/home/home-controller.js
@@ -5,9 +5,22 @@ module.controller("HomeController", ["$scope", function($scope) {
     $scope.navigatePage = function(page) {
         if(this.page !== page) {
             this.page = page;
+            var scrollTo = this.page === "login" ? document.querySelector(".link-panel").offsetTop : 0;
+            window.scrollTo(0, scrollTo);
         }
     }
     $scope.submitLogin = function() {
 
+    }
+
+    var header = document.querySelector(".fixed-header");
+    window.onscroll = function() {
+        if(header) {
+            if(window.scrollY >= header.offsetTop) {
+                document.body.classList.add("landing-fixed-header");
+            } else if(window.scrollY < header.offsetTop) {
+                document.body.classList.remove("landing-fixed-header");
+            }
+        }
     }
 }]);
